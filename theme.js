e3dfc634625d3613dc7403bc26682618a840fb1c
Alexandr Iurchenko <104443262+linuxseller@users.noreply.github.com>
2025-02-06 00:51:46 +0700

    Make dark theme default

--- theme.js
+++ theme.js
@@ -20,12 +20,12 @@ const darkModeSVG = `<svg height="32px" width="32px" version="1.1" id="Capa_1" x
 
 window.onload = () => {
     const theme = localStorage.getItem('theme');
-    if(theme === 'dark'){
-        document.body.classList.remove('light');
-        document.body.classList.add('dark');
-    }else{
+    if(theme === 'light'){
         document.body.classList.remove('dark');
         document.body.classList.add('light');
+    }else{
+        document.body.classList.remove('light');
+        document.body.classList.add('dark');
     }
     const button = document.createElement('button');
     button.innerHTML = theme === 'dark' ? darkModeSVG : lightModeSVG;
